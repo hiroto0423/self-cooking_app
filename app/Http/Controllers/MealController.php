82507bc620301aa1e93344d27450d29700bc9ae1
hiroto0423 <rohito.0423-tkh@outlook.jp>
2022-11-20 17:55:43 +0900

    migrationファイル作成

---
+++ app/Http/Controllers/MealController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Meal;
+use Illuminate\Http\Request;
+
+class MealController extends Controller
+{
+    
+}
+
---
+++ database/migrations/2022_11_20_162601_create_meals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',20);
+            $table->integer('categoly_id');
+            $table->integer('user_id');
+            $table->integer('img_id');
+            $table->integer('cost');
+            $table->integer('difficulty');
+            $table->text('way',200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+}
