309354f59ee79f7cf5bbe5b6aa2a5e7a23948f6a
hiroto0423 <rohito.0423-tkh@outlook.jp>
2022-11-20 21:36:44 +0900

    model　作成

---
+++ app/Models/Img.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Img extends Model
+{
+    use HasFactory;
+    protected $guarded = array('id');
+
+    public function todo(){
+        return $this->hasOne('App\Models\Todo');
+    }
+}
--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Meal extends Model
 {
     use HasFactory;
+    protected $guarded = array('id');
+
+    public function user()
+    {
+        return $this->BelongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->BelongsTo(category::class);
+    }
+
+    public function img()
+    {
+        return $this->BelongsTo(Img::class);
+    }
 }
