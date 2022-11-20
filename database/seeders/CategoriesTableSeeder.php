d34b7936b77c461c9b10872339cd1a559f95ab77
hiroto0423 <rohito.0423-tkh@outlook.jp>
2022-11-20 23:35:58 +0900

    categoryテーブルシーディングの作成"

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => '肉料理'
+        ];
+        Category::create($param);
+
+        $param = [
+            'name' => '魚料理'
+        ];
+        Category::create($param);
+
+        $param = [
+            'name' => 'サラダ'
+        ];
+        Category::create($param);
+
+        $param = [
+            'name' => 'デザート'
+        ];
+        Category::create($param);
+    }
+}
