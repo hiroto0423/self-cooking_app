a45166f02323f3dd3967cb978e830a811cb8a124
hiroto0423 <rohito.0423-tkh@outlook.jp>
2022-11-22 02:02:39 +0900

    header 作成

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,1 +1,28 @@
-headerです
+<header>
+  <div class="header-logo">
+    <img src="{{ asset('selcook icon.png') }}" alt="app-icon" class="header-icon">
+    <h1 class="header-logo__ttl">selcook</h1>
+  </div>
+  <nav class="header-log__nav">
+    <ul class="header-log__list flex">
+      <li class="header-log__item">
+        <a href="/">トップ</a>
+      </li>
+      <li class="header-log__item">
+        <a href="">投稿</a>
+      </li>
+      <li class="header-log__item">
+        <a href="/meals">レシピ一覧</a>
+      </li>
+      <li class="header-log__item">
+        <a href="/meals/create">レシピ作成</a>
+      </li>
+      <li class="header-log__item">
+        <form method="post" action="/logout">
+          @csrf
+          <input class="btn-logout" type="submit" value="ログアウト">
+        </form>
+      </li>
+    </ul>
+  </nav>
+</header>
--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -5,8 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>自炊継続アプリ</title>
-  <link rel="stylesheet" href="../css/reset.css" />
-  <link rel="stylesheet" href="../css/style.css" />
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/header.css') }}" />
 </head>
 <body>
 @component('components.header')
