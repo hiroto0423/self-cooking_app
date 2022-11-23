15c8b1d882d2ad4379581755ece2cc2f23f552b1
hiroto0423 <rohito.0423-tkh@outlook.jp>
2022-11-23 17:51:05 +0900

    meal-app heroku commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,8 +39,8 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
+            <a href="/register"style="padding-right:30px;">登録はこちら</a>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
